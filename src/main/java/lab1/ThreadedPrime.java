5ea017a108df88824c206fc73f93ce60396dec3f
@seseblante <seseblante@up.edu.ph>
2024-03-13 23:19:38 +0800

    Testing commit

--- src/main/java/lab1/ThreadedPrime.java
+++ src/main/java/lab1/ThreadedPrime.java
@@ -2,20 +2,20 @@ package lab1;
 
 import java.util.*;
 
-public class ThreadedPrime {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        ThreadedPrime threadedPrime = new ThreadedPrime();
-        List<Integer> primes = threadedPrime.getListOfPrimes(0,null);
-        threadedPrime.printListOfPrimes(primes);
-    }
-
-    private List<Integer> getListOfPrimes(int numberOfThreads, Integer maxNumber) {
-
-        return null;
-    }
-
-    private void printListOfPrimes(List<Integer> primes) {
-
-    }}
+//public class ThreadedPrime {
+////    public static void main(String[] args) {
+////        System.out.println("Hello world!");
+////
+////        ThreadedPrime threadedPrime = new ThreadedPrime();
+////        List<Integer> primes = threadedPrime.getListOfPrimes(0,null);
+////        threadedPrime.printListOfPrimes(primes);
+////    }
+//
+//    private List<Integer> getListOfPrimes(int numberOfThreads, Integer maxNumber) {
+//
+//        return null;
+//    }
+//
+//    private void printListOfPrimes(List<Integer> primes) {
+//
+//    }}
